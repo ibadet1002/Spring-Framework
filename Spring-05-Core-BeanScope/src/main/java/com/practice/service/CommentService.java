60fd94d564433a4326e14a681479edf06cf88b53
ibadet1002 <ibadet1002@gmail.com>
2022-03-03 20:06:23 -0500

    make up

---
+++ Spring-05-Core-BeanScope/src/main/java/com/practice/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.practice.service;
+
+import com.practice.model.Comment;
+import com.practice.proxy.CommentNotificationProxy;
+import com.practice.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
+
--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+
 @Controller
 public class StudentController {
 
@@ -13,6 +19,21 @@ public class StudentController {
         model.addAttribute("state","Virginia");
         model.addAttribute("City","Chantilly");
 
+        String county = "Fairfax county";
+        model.addAttribute("county", county);
+
+        // create some random student id, display on ui
+
+        int id = new Random().nextInt(1-1000);
+        model.addAttribute("id", id);
+
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(3);
+        model.addAttribute("numbers", numbers)
+
+;
         return"/student/welcome";
 
     }
