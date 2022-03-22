0405b782695bba1061380a28f4a259b4a4037fd7
ibadet1002 <ibadet1002@gmail.com>
2022-03-22 19:44:37 -0400

    Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -4,22 +4,26 @@ import com.example.bootstrap.DataGenerator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    //    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
-  model.addAttribute("students", DataGenerator.createStudent());
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
-    public String info(Model model){
-
+    //    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
+    public String info(){
         return "student/welcome";
     }
-
 }
+
