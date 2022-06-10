3ee28e50b55d0c769aa3f96ba2b90277d500bfa5
ibadet1002 <ibadet1002@gmail.com>
2022-06-09 20:23:51 -0400

    Jackson

---
+++ Spring-16-REST-Jackson/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.entity.Account;
+import com.example.entity.User;
+import com.example.repository.AccountRepository;
+import com.example.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+    private UserRepository userRepository;
+    private AccountRepository accountRepository;
+
+    public HomeController(UserRepository userRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> readAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> readAllAccounts(){
+        return accountRepository.findAll();
+    }
+}
