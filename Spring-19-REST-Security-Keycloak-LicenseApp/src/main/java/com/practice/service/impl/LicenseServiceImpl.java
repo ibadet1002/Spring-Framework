ebe18cd141ce8df3e828f058e02b60c638450702
ibadet1002 <ibadet1002@gmail.com>
2022-07-25 00:02:11 -0400

    Keycloak License App

---
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/practice/service/impl/LicenseServiceImpl.java
@@ -0,0 +1,66 @@
+package com.practice.service.impl;
+
+import com.practice.model.License;
+import com.practice.model.Organization;
+import com.practice.repository.LicenseRepository;
+import com.practice.client.OrganizationFeignClient;
+import com.practice.service.LicenseService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LicenseServiceImpl implements LicenseService {
+
+    private final LicenseRepository licenseRepository;
+    private final OrganizationFeignClient feignClient;
+
+    public LicenseServiceImpl(LicenseRepository licenseRepository, OrganizationFeignClient feignClient) {
+        this.licenseRepository = licenseRepository;
+        this.feignClient = feignClient;
+    }
+
+    public License getLicense(Long licenseId, Long organizationId) throws Exception {
+
+        License license = licenseRepository.findById(licenseId).orElseThrow(() -> new Exception("License not found"));
+
+        Organization organization = retrieveOrganizationInfo(organizationId);
+
+        if (organization != null) {
+
+            license.setOrganizationName(organization.getName());
+            license.setContactName(organization.getContactName());
+            license.setContactEmail(organization.getContactEmail());
+            license.setContactPhone(organization.getContactPhone());
+
+        }
+
+        return license;
+
+    }
+
+    private Organization retrieveOrganizationInfo(Long organizationId) {
+        return feignClient.getOrganization(organizationId);
+    }
+
+    public License createLicense(License license) {
+        licenseRepository.save(license);
+        return license;
+    }
+
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
+        return license;
+    }
+
+    public String deleteLicense(Long licenseId) throws Exception {
+        License license = licenseRepository.findById(licenseId).orElseThrow(() -> new Exception("License not found"));
+        licenseRepository.delete(license);
+        return "License deleted";
+    }
+
+    public List<License> getLicensesByOrganization(Long organizationId) {
+        return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+}
---
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/test/java/com/practice/Spring19RestSecurityKeycloakLicenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring19RestSecurityKeycloakLicenseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
