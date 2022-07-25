7014f20266554d3cab1423caf75f3684496612ba
ibadet1002 <ibadet1002@gmail.com>
2022-07-24 23:30:27 -0400

    OpenApi

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/practice/repository/CinemaRepository.java
@@ -0,0 +1,9 @@
+package com.practice.repository;
+
+import com.practice.entity.Cinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+}
